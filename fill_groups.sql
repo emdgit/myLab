9b423bd2442b2e48825224f4c20d74a9f43cc8f9
emdgit <emilyenn@yandex.ru>
2020-06-25 02:15:14 +0300

    Add 2 fill-sql scripts

---
+++ fill_groups.sql
@@ -0,0 +1,21 @@
+﻿select * from common.add_group('Продукты', NULL);
+select * from common.add_group('Напитки', NULL);
+select * from common.add_group('Одежда', NULL);
+select * from common.add_group('Связь', NULL);
+select * from common.add_group('Транспорт', NULL);
+
+select * from common.add_group('Мсяо', 1);
+select * from common.add_group('Кукурузица', 1);
+select * from common.add_group('Тортик', 1);
+
+select * from common.add_group('Пиво', 2);
+select * from common.add_group('Водка', 2);
+
+select * from common.add_group('Штанцы', 3);
+select * from common.add_group('Рубахи', 3);
+
+select * from common.add_group('теле-2', 4);
+select * from common.add_group('мтс', 4);
+
+select * from common.add_group('Каршеринг', 5);
+select * from common.add_group('Метро', 5);
---
+++ fill_records.sql
@@ -0,0 +1,10 @@
+﻿select * from common.add_user('developer');
+
+-- ugroupID, name
+select * from common.add_record(6, 'котлетосы');
+select * from common.add_record(6, 'курятина');
+select * from common.add_record(6, 'колбаса');
+
+select * from common.add_record(15, 'яндекс');
+select * from common.add_record(15, 'белка');
+select * from common.add_record(15, 'делимобиль');
